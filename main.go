2de6e019e4bfe4cfdbe9ec6142365169cb8cc54b
hiroaki_tateishi@densan-s.co.jp <hiroaki_tateishi@densan-s.co.jp>
2018-11-02 14:47:13 +0900

    first

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "net/http"
+    "encoding/json"
+
+    "google.golang.org/appengine"
+)
+
+type Response struct {
+    Status  string `json:"status"`
+    Message string `json:"message"`
+}
+
+func main() {
+    http.HandleFunc("/", handle)
+    appengine.Main()
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(Response{Status: "ok", Message: "cloud build"})
+}
